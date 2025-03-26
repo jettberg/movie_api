a7b4eacf1fe701cf1f47fed2f67d2fa446f499ee
jettberg <164446552+jettberg@users.noreply.github.com>
2025-03-26 13:16:03 -0500

    took off the other auth

--- index.js
+++ index.js
@@ -66,148 +66,148 @@ let users = [
 
 ];
 const movies = [
-  {
-    "Title": "Inception",
-    "Description": "A skilled thief who steals corporate secrets through the use of dream-sharing technology is given the chance to have his criminal record erased if he can successfully perform an inception.",
-    "Genre": {
-      "Name": "Science Fiction, Action, Thriller",
-      "Description": "In film and television, Science Fiction is a genre that uses speculative, futuristic, or imaginative concepts to explore possible futures, other worlds, or advanced technology."
-    },
-    "Director": {
-      "Name": "Christopher Nolan",
-      "Bio": "Christopher Nolan is a British-American filmmaker known for his complex narratives and visual storytelling. He has directed iconic films like The Dark Knight trilogy and Inception.",
-      "Birth": "July 30, 1970"
-    },
-    "Featured": false
-  },
-  {
-    "Title": "The Lord of the Rings: The Fellowship of the Ring",
-    "Description": "A young hobbit, Frodo Baggins, is tasked with the responsibility of destroying the powerful One Ring in the fires of Mount Doom, accompanied by a group of companions.",
-    "Genre": {
-      "Name": "Fantasy, Adventure",
-      "Description": "Fantasy is a genre of speculative fiction involving magical elements, mythical creatures, and supernatural occurrences, while Adventure stories focus on the pursuit of exciting and often dangerous quests."
-    },
-    "Director": {
-      "Name": "Peter Jackson",
-      "Bio": "Peter Jackson is a New Zealand filmmaker best known for directing The Lord of the Rings trilogy and The Hobbit series, both adapted from J.R.R. Tolkien's novels.",
-      "Birth": "October 31, 1961"
-    },
-    "Featured": true
-  },
-  {
-    "Title": "The Lord of the Rings: The Two Towers",
-    "Description": "Frodo and Sam continue their perilous journey to Mount Doom, while the remaining members of the Fellowship prepare for war against the forces of Sauron.",
-    "Genre": {
-      "Name": "Fantasy, Adventure",
-      "Description": "Fantasy is a genre of speculative fiction involving magical elements, mythical creatures, and supernatural occurrences, while Adventure stories focus on the pursuit of exciting and often dangerous quests."
-    },
-    "Director": {
-      "Name": "Peter Jackson",
-      "Bio": "Peter Jackson is a New Zealand filmmaker best known for directing The Lord of the Rings trilogy and The Hobbit series, both adapted from J.R.R. Tolkien's novels.",
-      "Birth": "October 31, 1961"
-    },
-    "Featured": true
-  },
-  {
-    "Title": "The Lord of the Rings: The Return of the King",
-    "Description": "The final battle to destroy the One Ring takes place as Frodo struggles to reach Mount Doom while the armies of Middle-Earth fight to hold off Sauron's forces.",
-    "Genre": {
-      "Name": "Fantasy, Adventure",
-      "Description": "Fantasy is a genre of speculative fiction involving magical elements, mythical creatures, and supernatural occurrences, while Adventure stories focus on the pursuit of exciting and often dangerous quests."
-    },
-    "Director": {
-      "Name": "Peter Jackson",
-      "Bio": "Peter Jackson is a New Zealand filmmaker best known for directing The Lord of the Rings trilogy and The Hobbit series, both adapted from J.R.R. Tolkien's novels.",
-      "Birth": "October 31, 1961"
-    },
-    "Featured": true
-  },
-  {
-    "Title": "The Hobbit: An Unexpected Journey",
-    "Description": "Bilbo Baggins, a hobbit, embarks on a journey with a group of dwarves to reclaim a stolen treasure from the dragon Smaug.",
-    "Genre": {
-      "Name": "Fantasy, Adventure",
-      "Description": "Fantasy is a genre of speculative fiction involving magical elements, mythical creatures, and supernatural occurrences, while Adventure stories focus on the pursuit of exciting and often dangerous quests."
-    },
-    "Director": {
-      "Name": "Peter Jackson",
-      "Bio": "Peter Jackson is a New Zealand filmmaker best known for directing The Lord of the Rings trilogy and The Hobbit series, both adapted from J.R.R. Tolkien's novels.",
-      "Birth": "October 31, 1961"
-    },
-    "Featured": false
-  },
-  {
-    "Title": "The Hobbit: The Desolation of Smaug",
-    "Description": "Bilbo Baggins and the dwarves continue their journey to reclaim the treasure of the Lonely Mountain, encountering danger and the dragon Smaug.",
-    "Genre": {
-      "Name": "Fantasy",
-      "Description": "Fantasy is a genre of speculative fiction involving magical elements, mythical creatures, and supernatural occurrences, while Adventure stories focus on the pursuit of exciting and often dangerous quests."
-    },
-    "Director": {
-      "Name": "Peter Jackson",
-      "Bio": "Peter Jackson is a New Zealand filmmaker best known for directing The Lord of the Rings trilogy and The Hobbit series, both adapted from J.R.R. Tolkien's novels.",
-      "Birth": "October 31, 1961"
-    },
-    "Featured": false
-  },
-  {
-    "Title": "The Hobbit: The Battle of the Five Armies",
-    "Description": "The final chapter of the Hobbit trilogy, where Bilbo Baggins and his companions must face the repercussions of their actions as the forces of good and evil converge for the Battle of the Five Armies.",
-    "Genre": {
-      "Name": "Fantasy, Adventure",
-      "Description": "Fantasy is a genre of speculative fiction involving magical elements, mythical creatures, and supernatural occurrences, while Adventure stories focus on the pursuit of exciting and often dangerous quests."
-    },
-    "Director": {
-      "Name": "Peter Jackson",
-      "Bio": "Peter Jackson is a New Zealand filmmaker best known for directing The Lord of the Rings trilogy and The Hobbit series, both adapted from J.R.R. Tolkien's novels.",
-      "Birth": "October 31, 1961"
-    },
-    "Featured": false
-  },
-  {
-    "Title": "Spaceman",
-    "Description": "A space explorer must navigate life in an isolated mission on a distant planet, facing both external dangers and his internal battles. Adam Sandler plays a comedic yet emotional lead role.",
-    "Genre": {
-      "Name": "Comedy, Sci-Fi, Drama",
-      "Description": "Comedy is a genre of film designed to entertain and make the audience laugh, Sci-Fi involves speculative futuristic concepts, and Drama explores emotional themes with a focus on character development."
-    },
-    "Director": {
-      "Name": "Jonah Hill",
-      "Bio": "Jonah Hill is an American actor, director, producer, and writer, known for his roles in films like Superbad, 21 Jump Street, and his directorial debut, Mid90s.",
-      "Birth": "December 20, 1983"
-    },
-    "Featured": false
-  },
-  {
-    "Title": "The Martian",
-    "Description": "Astronaut Mark Watney becomes stranded on Mars and must figure out how to survive while awaiting rescue. A gripping survival story of ingenuity and hope.",
-    "Genre": {
-      "Name": "Science Fiction, Drama",
-      "Description": "Science Fiction involves imaginative future scenarios often dealing with advanced technologies or alien life, while Drama focuses on realistic storytelling and emotional conflict."
-    },
-    "Director": {
-      "Name": "Ridley Scott",
-      "Bio": "Ridley Scott is an English director, producer, and screenwriter, known for his work on films such as Alien, Gladiator, and The Martian.",
-      "Birth": "November 30, 1937"
-    },
-    "Featured": false
-  },
-  {
-    "Title": "Shutter Island",
-    "Description": "A U.S. Marshal investigates the disappearance of a patient from a mental hospital located on an isolated island, only to uncover disturbing truths about the island's secrets.",
-    "Genre": {
-      "Name": "Mystery, Thriller",
-      "Description": "Mystery is a genre involving a crime or puzzle to be solved, and Thriller is focused on intense excitement and suspense, often with psychological elements."
-    },
-    "Director": {
-      "Name": "Martin Scorsese",
-      "Bio": "Martin Scorsese is an American film director, producer, screenwriter, and actor, widely regarded as one of the greatest directors in the history of cinema.",
-      "Birth": "November 17, 1942"
-    },
-    "Featured": false
-  }
+//   {
+//     "Title": "Inception",
+//     "Description": "A skilled thief who steals corporate secrets through the use of dream-sharing technology is given the chance to have his criminal record erased if he can successfully perform an inception.",
+//     "Genre": {
+//       "Name": "Science Fiction, Action, Thriller",
+//       "Description": "In film and television, Science Fiction is a genre that uses speculative, futuristic, or imaginative concepts to explore possible futures, other worlds, or advanced technology."
+//     },
+//     "Director": {
+//       "Name": "Christopher Nolan",
+//       "Bio": "Christopher Nolan is a British-American filmmaker known for his complex narratives and visual storytelling. He has directed iconic films like The Dark Knight trilogy and Inception.",
+//       "Birth": "July 30, 1970"
+//     },
+//     "Featured": false
+//   },
+//   {
+//     "Title": "The Lord of the Rings: The Fellowship of the Ring",
+//     "Description": "A young hobbit, Frodo Baggins, is tasked with the responsibility of destroying the powerful One Ring in the fires of Mount Doom, accompanied by a group of companions.",
+//     "Genre": {
+//       "Name": "Fantasy, Adventure",
+//       "Description": "Fantasy is a genre of speculative fiction involving magical elements, mythical creatures, and supernatural occurrences, while Adventure stories focus on the pursuit of exciting and often dangerous quests."
+//     },
+//     "Director": {
+//       "Name": "Peter Jackson",
+//       "Bio": "Peter Jackson is a New Zealand filmmaker best known for directing The Lord of the Rings trilogy and The Hobbit series, both adapted from J.R.R. Tolkien's novels.",
+//       "Birth": "October 31, 1961"
+//     },
+//     "Featured": true
+//   },
+//   {
+//     "Title": "The Lord of the Rings: The Two Towers",
+//     "Description": "Frodo and Sam continue their perilous journey to Mount Doom, while the remaining members of the Fellowship prepare for war against the forces of Sauron.",
+//     "Genre": {
+//       "Name": "Fantasy, Adventure",
+//       "Description": "Fantasy is a genre of speculative fiction involving magical elements, mythical creatures, and supernatural occurrences, while Adventure stories focus on the pursuit of exciting and often dangerous quests."
+//     },
+//     "Director": {
+//       "Name": "Peter Jackson",
+//       "Bio": "Peter Jackson is a New Zealand filmmaker best known for directing The Lord of the Rings trilogy and The Hobbit series, both adapted from J.R.R. Tolkien's novels.",
+//       "Birth": "October 31, 1961"
+//     },
+//     "Featured": true
+//   },
+//   {
+//     "Title": "The Lord of the Rings: The Return of the King",
+//     "Description": "The final battle to destroy the One Ring takes place as Frodo struggles to reach Mount Doom while the armies of Middle-Earth fight to hold off Sauron's forces.",
+//     "Genre": {
+//       "Name": "Fantasy, Adventure",
+//       "Description": "Fantasy is a genre of speculative fiction involving magical elements, mythical creatures, and supernatural occurrences, while Adventure stories focus on the pursuit of exciting and often dangerous quests."
+//     },
+//     "Director": {
+//       "Name": "Peter Jackson",
+//       "Bio": "Peter Jackson is a New Zealand filmmaker best known for directing The Lord of the Rings trilogy and The Hobbit series, both adapted from J.R.R. Tolkien's novels.",
+//       "Birth": "October 31, 1961"
+//     },
+//     "Featured": true
+//   },
+//   {
+//     "Title": "The Hobbit: An Unexpected Journey",
+//     "Description": "Bilbo Baggins, a hobbit, embarks on a journey with a group of dwarves to reclaim a stolen treasure from the dragon Smaug.",
+//     "Genre": {
+//       "Name": "Fantasy, Adventure",
+//       "Description": "Fantasy is a genre of speculative fiction involving magical elements, mythical creatures, and supernatural occurrences, while Adventure stories focus on the pursuit of exciting and often dangerous quests."
+//     },
+//     "Director": {
+//       "Name": "Peter Jackson",
+//       "Bio": "Peter Jackson is a New Zealand filmmaker best known for directing The Lord of the Rings trilogy and The Hobbit series, both adapted from J.R.R. Tolkien's novels.",
+//       "Birth": "October 31, 1961"
+//     },
+//     "Featured": false
+//   },
+//   {
+//     "Title": "The Hobbit: The Desolation of Smaug",
+//     "Description": "Bilbo Baggins and the dwarves continue their journey to reclaim the treasure of the Lonely Mountain, encountering danger and the dragon Smaug.",
+//     "Genre": {
+//       "Name": "Fantasy",
+//       "Description": "Fantasy is a genre of speculative fiction involving magical elements, mythical creatures, and supernatural occurrences, while Adventure stories focus on the pursuit of exciting and often dangerous quests."
+//     },
+//     "Director": {
+//       "Name": "Peter Jackson",
+//       "Bio": "Peter Jackson is a New Zealand filmmaker best known for directing The Lord of the Rings trilogy and The Hobbit series, both adapted from J.R.R. Tolkien's novels.",
+//       "Birth": "October 31, 1961"
+//     },
+//     "Featured": false
+//   },
+//   {
+//     "Title": "The Hobbit: The Battle of the Five Armies",
+//     "Description": "The final chapter of the Hobbit trilogy, where Bilbo Baggins and his companions must face the repercussions of their actions as the forces of good and evil converge for the Battle of the Five Armies.",
+//     "Genre": {
+//       "Name": "Fantasy, Adventure",
+//       "Description": "Fantasy is a genre of speculative fiction involving magical elements, mythical creatures, and supernatural occurrences, while Adventure stories focus on the pursuit of exciting and often dangerous quests."
+//     },
+//     "Director": {
+//       "Name": "Peter Jackson",
+//       "Bio": "Peter Jackson is a New Zealand filmmaker best known for directing The Lord of the Rings trilogy and The Hobbit series, both adapted from J.R.R. Tolkien's novels.",
+//       "Birth": "October 31, 1961"
+//     },
+//     "Featured": false
+//   },
+//   {
+//     "Title": "Spaceman",
+//     "Description": "A space explorer must navigate life in an isolated mission on a distant planet, facing both external dangers and his internal battles. Adam Sandler plays a comedic yet emotional lead role.",
+//     "Genre": {
+//       "Name": "Comedy, Sci-Fi, Drama",
+//       "Description": "Comedy is a genre of film designed to entertain and make the audience laugh, Sci-Fi involves speculative futuristic concepts, and Drama explores emotional themes with a focus on character development."
+//     },
+//     "Director": {
+//       "Name": "Jonah Hill",
+//       "Bio": "Jonah Hill is an American actor, director, producer, and writer, known for his roles in films like Superbad, 21 Jump Street, and his directorial debut, Mid90s.",
+//       "Birth": "December 20, 1983"
+//     },
+//     "Featured": false
+//   },
+//   {
+//     "Title": "The Martian",
+//     "Description": "Astronaut Mark Watney becomes stranded on Mars and must figure out how to survive while awaiting rescue. A gripping survival story of ingenuity and hope.",
+//     "Genre": {
+//       "Name": "Science Fiction, Drama",
+//       "Description": "Science Fiction involves imaginative future scenarios often dealing with advanced technologies or alien life, while Drama focuses on realistic storytelling and emotional conflict."
+//     },
+//     "Director": {
+//       "Name": "Ridley Scott",
+//       "Bio": "Ridley Scott is an English director, producer, and screenwriter, known for his work on films such as Alien, Gladiator, and The Martian.",
+//       "Birth": "November 30, 1937"
+//     },
+//     "Featured": false
+//   },
+//   {
+//     "Title": "Shutter Island",
+//     "Description": "A U.S. Marshal investigates the disappearance of a patient from a mental hospital located on an isolated island, only to uncover disturbing truths about the island's secrets.",
+//     "Genre": {
+//       "Name": "Mystery, Thriller",
+//       "Description": "Mystery is a genre involving a crime or puzzle to be solved, and Thriller is focused on intense excitement and suspense, often with psychological elements."
+//     },
+//     "Director": {
+//       "Name": "Martin Scorsese",
+//       "Bio": "Martin Scorsese is an American film director, producer, screenwriter, and actor, widely regarded as one of the greatest directors in the history of cinema.",
+//       "Birth": "November 17, 1942"
+//     },
+//     "Featured": false
+//   }
+// ];
 ];
-
 
 
 
@@ -228,7 +228,7 @@ app.get('/documentation', (req, res) => {
 // app.get('/movies', (req, res) => {
 //   res.status(200).json(movies);
 // });
-app.get('/movies', passport.authenticate('jwt', {session: false}), async (req, res) => {
+app.get('/movies', async (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
