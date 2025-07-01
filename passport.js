8605d79b50333c35f2041a29d5a029d38a7858e6
jettberg <164446552+jettberg@users.noreply.github.com>
2025-06-30 21:27:22 -0500

    signup fix.3

--- passport.js
+++ passport.js
@@ -50,7 +50,7 @@ passport.use(
 
 passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: 'your_jwt_secret'
+    secretOrKey: jwtSecret
 }, async (jwtPayload, callback) => {
     return await Users.findById(jwtPayload._id)
         .then((user) => {
